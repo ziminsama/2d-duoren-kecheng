43828fbaf34dad897573c3b4132fac17d69f92d8
ziminsama <191540376@qq.com>
2026-07-16 16:51:48 +0800

    A6 10:27/15:45

--- main.gd
+++ main.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+
+func _ready() -> void:
+	print("hello world peer_ready.rpc")
+	peer_ready.rpc_id(1)
+
+
+@rpc("any_peer","call_remote","reliable")
+func peer_ready():
+	print("peer %s ready" % multiplayer.get_remote_sender_id())
+	
