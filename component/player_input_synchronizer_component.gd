b25927798c9650ebb804439d16ce8284cb180c1e
ziminsama <191540376@qq.com>
2026-07-17 16:57:17 +0800

    A10-11 增加美术和武器

--- component/player_input_synchronizer_component.gd
+++ component/player_input_synchronizer_component.gd
@@ -1,7 +1,12 @@
 class_name PlayerInputSynchronizerComponent
 extends MultiplayerSynchronizer
 
+
+@export var aim_root: Node2D
+
+
 var movement_vector: Vector2 = Vector2.ZERO
+var aim_vector: Vector2 = Vector2.RIGHT
 
 func _process(_delta: float) -> void:
 	if is_multiplayer_authority():
@@ -10,3 +15,4 @@ func _process(_delta: float) -> void:
 
 func gather_input():
 	movement_vector = Input.get_vector("move_left","move_right","move_up","move_down")
+	aim_vector = aim_root.global_position.direction_to(aim_root.get_global_mouse_position())
--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,18 +2,25 @@ class_name Player
 extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
+@onready var weapon_root: Node2D = $WeaponRoot
+
 
 var input_multiplayer_authority: int
 
 
 func _ready() -> void:
 	player_input_synchronizer_component.set_multiplayer_authority(input_multiplayer_authority)
-	set_process(is_multiplayer_authority())
 
 
 func _process(_delta: float) -> void:
-	velocity = player_input_synchronizer_component.movement_vector * 300
-	move_and_slide()
+	var aim_position = weapon_root.global_position \
+		+ player_input_synchronizer_component.aim_vector
+		#用\可以在下一行写，下一行加一个缩进影响，但能更好看
+	weapon_root.look_at(aim_position)
+	
+	if is_multiplayer_authority():
+		velocity = player_input_synchronizer_component.movement_vector * 300
+		move_and_slide()
 
 
 #测试 建议用_physics_process,我在测有什么不同,实际发现没啥不同
