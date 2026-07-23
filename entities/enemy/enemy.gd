00d45e1aa422f9f82e86a82a90c43ce481ad2f6b
ziminsama <191540376@qq.com>
2026-07-23 23:24:51 +0800

    A21-22带寻敌的敌人,回合生成

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -2,18 +2,51 @@ extends CharacterBody2D
 
 
 @onready var area_2d: Area2D = $Area2D
+@onready var target_acquisition_timer: Timer = $TargetAcquisitionTimer
+
 
 var current_health: int = 1
+var target_position: Vector2
 
 
 func _ready() -> void:
 	area_2d.area_entered.connect(_on_area_entered)
+	target_acquisition_timer.timeout.connect(_on_target_acquisition_timer_timeout)
+	#每0.2秒拿一次
+	if is_multiplayer_authority():
+		acquire_target()
+
+#这里的怪物追敌AI可以延伸类比至法术追踪目标了,只是法术速度更快
+func _process(delta: float) -> void:
+	if is_multiplayer_authority():
+		velocity = global_position.direction_to(target_position) * 40
+		move_and_slide()
 
 
 func handel_hit():
 	current_health -= 1
 	if current_health <= 0:
 		queue_free()
+
+
+func acquire_target():
+	var players = get_tree().get_nodes_in_group("player")
+	var nearest_player: Player
+	var nearest_squared_distance: float
+	
+	for player in players:
+		if nearest_player == null:
+			nearest_player = player
+			nearest_squared_distance = nearest_player.global_position.distance_squared_to(global_position)
+			continue
+	
+		var player_squared_distance: float = player.global_position.distance_squared_to(global_position)
+		if player_squared_distance <= nearest_squared_distance:
+			nearest_squared_distance = player_squared_distance
+			nearest_player = player
+	
+	if nearest_player != null:
+		target_position = nearest_player.global_position
 
 
 func _on_area_entered(other_area: Area2D):
@@ -25,3 +58,10 @@ func _on_area_entered(other_area: Area2D):
 		bullet.register_collision()
 		handel_hit()
 		#print("collision")
+
+
+func _on_target_acquisition_timer_timeout():
+	if is_multiplayer_authority():
+		acquire_target()
+#这里应该是作者的习惯,每个信号有一个形象的触发函数,函数里面才是真正的函数,可以方便加入服务器权威判断
+#另外一个ryan的教程是用navigation2d
--- manager/enemy_manager.gd
+++ manager/enemy_manager.gd
@@ -1,15 +1,33 @@
 extends Node
 
+const BASE_ROUND_TIME: int = 20
+const ROUND_GROWTH: int = 5
+const BASE_ENEMY_SPAWN_TIME: float = 2
+const ENEMY_SPAWN_TIME_GROWTH: float = -0.15
+
 
 @export var enemy_scene: PackedScene
 @export var enemy_spawn_root: Node
 @export var spawn_rect: ReferenceRect
 
 @onready var spawn_interval_timer: Timer = $SpawnIntervalTimer
+@onready var round_timer: Timer = $RoundTimer
 
+var round_count: int = 0
 
 func _ready() -> void:
 	spawn_interval_timer.timeout.connect(_on_spawn_interval_timer_timeout)
+	round_timer.timeout.connect(_on_round_timer_timeout)
+	begin_round()
+
+
+func begin_round():
+	round_count += 1
+	round_timer.wait_time = BASE_ROUND_TIME + ((round_count - 1) * ROUND_GROWTH)
+	round_timer.start()
+	
+	spawn_interval_timer.wait_time = BASE_ENEMY_SPAWN_TIME + ((round_count - 1) * ENEMY_SPAWN_TIME_GROWTH)
+	spawn_interval_timer.start()
 
 
 func get_randon_spawn_position() -> Vector2:
@@ -27,3 +45,10 @@ func spawn_enemy():
 func _on_spawn_interval_timer_timeout():
 	if is_multiplayer_authority():
 		spawn_enemy()
+		spawn_interval_timer.start()
+
+
+func _on_round_timer_timeout():
+	if is_multiplayer_authority():
+		spawn_interval_timer.stop()
+		print("round over")
