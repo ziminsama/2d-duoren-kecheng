e3df5d69779533b48567a50471715d198cf60276
ziminsama <191540376@qq.com>
2026-07-26 17:01:32 +0800

    B31-34武器射击动画枪口粒子
    武器贴图下建一个sprite2D通过旋转,缩放武器贴图,制作武器发射动画
    新建一个GPGParticles2D场景,制作粒子效果,在武器枪口跟随开火函数发射粒子特效

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -6,16 +6,17 @@ extends CharacterBody2D
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var visuals: Node2D = $Visuals
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var barrel_position: Marker2D = %BarrelPosition
 
 var bullet_scene: PackedScene = preload("uid://jlap4yf3gf0i")
+var muzzle_flash_scene: PackedScene = preload("uid://cnxo8p5hrj6tv")
 var input_multiplayer_authority: int
 
 
 func _ready() -> void:
 	player_input_synchronizer_component.set_multiplayer_authority(input_multiplayer_authority)
 	health_component.died.connect(_on_died)
-
-
 #func _input(event: InputEvent) -> void:
 	#if event.is_action_pressed("attack"):
 		#create_bullet()
@@ -33,7 +34,7 @@ func _process(_delta: float) -> void:
 	#velocity = movement_vector * 300
 	#move_and_slide()
 		if player_input_synchronizer_component.is_attack_pressed:
-			try_create_bullet()
+			try_fire()
 			#教学从一开始叫create_bullet无间隔，到后来加入timer有间隔，改成try_create_bullet，名字充分表达意图
 
 
@@ -43,20 +44,34 @@ func update_aim_position():
 	
 	visuals.scale = Vector2.ONE if aim_vector.x >=0 else Vector2(-1, 1)
 	weapon_root.look_at(aim_position)
-	
 
 
-func try_create_bullet():
+func try_fire():
 	if !fire_rate_timer.is_stopped():
 		return
+	
 	var bullet = bullet_scene.instantiate() as Bullet
 	#这个变量其实是场景实例化的节点,还在内存中,下一步再加到main场景中变成节点
-	bullet.global_position = weapon_root.global_position
+	bullet.global_position = barrel_position.global_position
 	#老师总结的经验是建议在添加节点之前改位置,这样会在_ready函数之前设好,如果添加了再改位置,就会运行了_ready之后再赋值全局位置
 	bullet.start(player_input_synchronizer_component.aim_vector)
 	#alt+↑是把代码上移，旋转和方向等自定义属性，可在节点加入场景树前先修改，有些是不行的。
 	get_parent().add_child(bullet, true)
 	fire_rate_timer.start()
+	
+	play_fire_effect.rpc()
+
+
+@rpc("authority", "call_local", "unreliable")
+func play_fire_effect():
+	if animation_player.is_playing():
+		animation_player.stop()
+	animation_player.play("fire")
+	
+	var muzzle_flash: Node2D = muzzle_flash_scene.instantiate()
+	muzzle_flash.global_position = barrel_position.global_position
+	muzzle_flash.rotation = barrel_position.global_rotation
+	get_parent().add_child(muzzle_flash)
 
 
 func _on_died():
